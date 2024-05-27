170d0035109ad20970ed657b1e21705ee96ffbae
Al Liu <scygliu1@gmail.com>
2024-05-27 20:04:03 +0800

    Fix fmt and CI

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,11 @@
 //! * [`ArrayStack`], a bounded stack that allocates a fixed-capacity buffer on construction.
 #![no_std]
 #![doc(test(
-    no_crate_inject,
-    attr(
-        deny(warnings, rust_2018_idioms, single_use_lifetimes),
-        allow(dead_code, unused_assignments, unused_variables)
-    )
+  no_crate_inject,
+  attr(
+    deny(warnings, rust_2018_idioms, single_use_lifetimes),
+    allow(dead_code, unused_assignments, unused_variables)
+  )
 ))]
 #![warn(missing_docs, unsafe_op_in_unsafe_fn)]
 
